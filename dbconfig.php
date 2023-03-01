c0e21e9a312f2a55655e5d9895c754e6ef4dbf27
Carlos_Henriquez _0505 <c.jose.h.mata@gmail.com>
2023-03-01 10:55:40 -0300

    3 archivos cambiados
    bbdd agregada
    dbconfig cambiado
    dbconnect cambiado

--- dbconfig.php
+++ dbconfig.php
@@ -1,6 +1,6 @@
 <?php
     $dbhost = 'localhost';
-    $dbusername= 'id19461560_chenrique_petscarlos';
-    $dbname = 'id19461560_tienda_petscarlos';
-    $dbpassword = 'hy?zg=zw!6YvNc3_';
+    $dbusername= 'root';
+    $dbname = 'tienda_petscarlos';
+    $dbpassword = '';
 ?>
--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 require 'dbconfig.php';
-$mysqli=new mysqli("localhost","id19461560_chenrique_petscarlos","B67CL4wm6HyC~zll","id19461560_tienda_petscarlos");
+$mysqli=new mysqli("localhost","root","",$dbname);
 $link = mysqli_connect($dbhost, $dbusername,$dbpassword,$dbname);
 if(mysqli_connect_errno()){
     printf("conexion fallida: %s\n", mysqli_connect_error());/*el porcentaje mostrara el tipo error que devuelve mysqli_connect_error  */
